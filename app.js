3140a6d432b822e0e411b3a746c9c316bcf178dc
healym3 <79485702+healym3@users.noreply.github.com>
2021-10-07 15:42:18 -0500

    add styles.css

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 var items = [];
 
 app.get("/", function(req, res) {
-  var today = new Date();
-  var options = {
+  let today = new Date();
+  let options = {
     weekday: 'long',
     day: 'numeric',
     month: 'long'
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
     dayOfWeek: day,
@@ -26,9 +27,7 @@ app.get("/", function(req, res) {
 app.post("/", function(req, res){
   console.log(req.body.newItem);
   items.push(req.body.newItem);
-  for(var j = 0; j < items.length; j++){
-    console.log(items[j]);
-  };
+
 
   res.redirect("/");
 });
