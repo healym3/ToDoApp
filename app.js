8a6ecd061bd0ec380837a208867d761559405dd4
healym3 <79485702+healym3@users.noreply.github.com>
2021-10-07 15:25:09 -0500

    add todolist functionality

--- app.js
+++ app.js
@@ -2,13 +2,37 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-app.get("/", function(req, res){
+var items = [];
 
+app.get("/", function(req, res) {
+  var today = new Date();
+  var options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {
+    dayOfWeek: day,
+    newListItems: items
+  });
 });
 
-app.listen(3000, function(){
+app.post("/", function(req, res){
+  console.log(req.body.newItem);
+  items.push(req.body.newItem);
+  for(var j = 0; j < items.length; j++){
+    console.log(items[j]);
+  };
+
+  res.redirect("/");
+});
+
+app.listen(3000, function() {
   console.log("Server running on port 3000.");
 })
